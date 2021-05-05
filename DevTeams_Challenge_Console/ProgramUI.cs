f3de075151cbb41399bff7707770261f9e014028
Chris Kronberg <ckronber@gmail.com>
2021-05-05 13:27:49 -0400

    Add project files.

---
+++ DevTeams_Challenge_Console/ProgramUI.cs
@@ -0,0 +1,26 @@
+﻿using DevTeams_Challenge_Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeams_Challenge_Console
+{
+    public class ProgramUI
+    {
+        //This class will be how we interact with our user through the console. When we need to access our data, we will call methods from our Repo class.
+
+        private DevTeamsRepo _repo = new DevTeamsRepo();
+
+        public void Run()
+        {
+            Menu();
+        }
+
+        private void Menu()
+        {
+            //Start with the main menu here
+        }
+    }
+}
